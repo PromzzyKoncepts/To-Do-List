40eaa8e55be003cbe65b84684c1f6e1ef56efc33
PromzzyKoncepts <okechukwupromise638@gmail.com>
2022-08-10 01:17:31 +0100

    Install linter and fix errors

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
-import _ from 'lodash';
 import './index.css';
 
-const listObject = {
-  description : '',
-  completed : true,
-  index : 0
-}
-const inputVal = document.getElementById('add')
+const listObject = [
+  {
+    description: 'wash the plates',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'clear the bushes',
+    completed: false,
+    index: 0,
+  },
+];
+
+// const inputVal = document.getElementById('add');
 
 const taskDisplay = () => {
   const addList = document.querySelector('.list-container');
-  const listDiv = document.createElement('div')
-  const listInput = document.createElement('label')
-  const Input = document.createElement('INPUT')
-  listInput.innerText = `${inputVal.value}`
-  
-  listDiv.appendChild(Input)
-  listDiv.appendChild(listInput)
-  addList.appendChild(listDiv)
-  
-  Input.setAttribute('type', 'checkbox')
-}
+  listObject.forEach((task) => {
+    const listDiv = document.createElement('div');
+    const listInput = document.createElement('label');
+    const Input = document.createElement('INPUT');
+    listInput.innerText = `${task.description}`;
+    // listInput.innerText = `${inputVal.value}`
+    Input.checked = `${task.completed}`;
 
-document.querySelector('#add').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      e = taskDisplay()
-      inputVal.value = ''
-    }
-});
+    listDiv.appendChild(Input);
+    listDiv.appendChild(listInput);
+    addList.appendChild(listDiv);
 
+    Input.setAttribute('type', 'checkbox');
+    Input.setAttribute('class', 'check');
+    listDiv.className = 'todos';
+  });
+};
 
+const onPageLoad = () => {
+  taskDisplay();
+};
+
+window.onload = onPageLoad();
+
+// document.querySelector('#add').addEventListener('keypress', function (e) {
+//     if (e.key === 'Enter') {
+//       e = taskDisplay()
+//       inputVal.value = ''
+//     }
+// });
